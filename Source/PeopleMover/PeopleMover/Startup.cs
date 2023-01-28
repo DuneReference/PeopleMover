c3920332c05f7e53965ad64ca544933accb6f948
Taylor Hoss <hosstay@hotmail.com>
2023-01-28 06:57:08 -0600

    Fix Modsettings not saving, added default labels, and changed +/- to sliders.

--- Source/PeopleMover/PeopleMover/Startup.cs
+++ Source/PeopleMover/PeopleMover/Startup.cs
@@ -13,18 +13,25 @@ namespace DuneRef_PeopleMover
 
     public class PeopleMoverSettings : ModSettings
     {
+        // movespeed
         public static int movespeedPathCost = 26;
+        public static int defaultMovespeedPathCost = 26;
 
+        // pathcost
         public static bool useExplicitPathingPathCost = false;
-        public static int pathingPathCost = 20;
+        public static bool defaultUseExplicitPathingPathCost = false;
 
+        public static int pathingPathCost = 20;
+        public static int defaultPathingPathCost = 20;
+
+        // debug
         public static bool showFlashingPathCost = false;
 
         public override void ExposeData()
         {
-            Scribe_Values.Look(ref movespeedPathCost, "additionalPathCost", movespeedPathCost);
-            Scribe_Values.Look(ref useExplicitPathingPathCost, "useExplicitPathingPathCost", useExplicitPathingPathCost);
-            Scribe_Values.Look(ref pathingPathCost, "pathingPathCost", pathingPathCost);
+            Scribe_Values.Look(ref movespeedPathCost, "additionalPathCost", defaultMovespeedPathCost);
+            Scribe_Values.Look(ref useExplicitPathingPathCost, "useExplicitPathingPathCost", defaultUseExplicitPathingPathCost);
+            Scribe_Values.Look(ref pathingPathCost, "pathingPathCost", defaultPathingPathCost);
             Scribe_Values.Look(ref showFlashingPathCost, "showFlashingPathCost", showFlashingPathCost);
             base.ExposeData();
         }
@@ -43,13 +50,13 @@ namespace DuneRef_PeopleMover
         {
             Listing_Standard listingStandard = new Listing_Standard();
             listingStandard.Begin(inRect);
-            listingStandard.Label("The speed of the mover. higher values is less walk speed.");
+            listingStandard.Label($"The speed of the mover. higher values of path cost translates to less walk speed difference. (Mod Default: {PeopleMoverSettings.defaultMovespeedPathCost})");
             listingStandard.Label($"{PeopleMoverSettings.movespeedPathCost} Path cost: ~{(38 - PeopleMoverSettings.movespeedPathCost) * 13} % walk speed");
-            listingStandard.IntAdjuster(ref PeopleMoverSettings.movespeedPathCost, 1, 0);
+            PeopleMoverSettings.movespeedPathCost = (int)listingStandard.Slider(PeopleMoverSettings.movespeedPathCost, 0f, 100f);
             listingStandard.Label("Advanced:");
-            listingStandard.CheckboxLabeled("Explicitly specify the pathCost increment of the mover.", ref PeopleMoverSettings.useExplicitPathingPathCost, "How much pawns prioritize/deprioritize walking on movers depending in direction.");
+            listingStandard.CheckboxLabeled($"Explicitly specify the pathCost increment of the mover. (Mod Default: {PeopleMoverSettings.defaultPathingPathCost})", ref PeopleMoverSettings.useExplicitPathingPathCost, "How much pawns prioritize/deprioritize walking on movers depending in direction.");
             listingStandard.Label($"{PeopleMoverSettings.pathingPathCost}");
-            listingStandard.IntAdjuster(ref PeopleMoverSettings.pathingPathCost, 1, -50);
+            PeopleMoverSettings.pathingPathCost = (int)listingStandard.Slider(PeopleMoverSettings.pathingPathCost, -50f, 100f);
             listingStandard.Label("Dev:");
             listingStandard.CheckboxLabeled("Show flashing pathCost cells.", ref PeopleMoverSettings.showFlashingPathCost);
             listingStandard.End();
